2d5f36591d8cd8fd82c3669b0ffdcbdd89e8610a
daltonbr <sacinopatinete@gmail.com>
2016-10-10 15:10:00 -0300

    Lab5 - part1 v0.3 [Aesthetic changes]

--- lab05/lab5part1.s
+++ lab05/lab5part1.s
@@ -1,7 +1,7 @@
 /***********************************************************
  * Lab 05 - UART and timers - oct, 5th, 2016               *
  * Part 1 - Flashing ascii chars on Altera Monitor Program *
- * version 0.2 - 10/10/16 (not tested)                     *
+ * version 0.3 - 10/10/16 (not tested)                     *
  * Authors:                                                *
  * Dalton Lima @daltonbr                                   *
  * Giovanna Cazelato @giovannaC                            *
@@ -20,7 +20,6 @@
 .equ WSPACE_UART_mask,          0xFF00          # only high halfword (imm16)
 .equ DATA_UART_mask,            0x00FF
 .equ UART_BASE_ADDRESS          0x10001000
-# .equ UART_CONTROL_ADDRESS       0x10001004
 .equ Z                          0x5A            # ASCII 90 (decimal)
 .equ COUNTER                    0x4C4B40        # 5*10ˆ6 [decimal]
 
@@ -28,27 +27,23 @@
     .global _start
 _start:
     movia   r8, UART_BASE_ADDRESS
-    # movia   r9, UART_CONTROL_ADDRESS
 
 /* Checking for Space in the FIFO for Writing (WSPACE) */
 POLLING_UART:
-	ldwio   r10, 4(r8)        	        # r10 = UART Control Register (entire word)
-    andhi   r10, r10, WSPACE_UART_mask
-    andi    r10, r10, 0x0000            # setting the r10 low halfword to 0
-    beq     r10, r0, POLLING_UART       # if (WSPACE == 0) loops
+	ldwio   r9, 4(r8)        	        # r9 = UART Control Register (entire word)
+    andhi   r9, r9, WSPACE_UART_mask
+    andi    r9, r9, 0x0000            # setting the r9 low halfword to 0
+    beq     r9, r0, POLLING_UART       # if (WSPACE == 0) loops
 
 /* 5*10ˆ6 cycles = 4secs (SDRAM) or 0.5 sec (SRAM) */
-    movia   r12, COUNTER                # initializing r12 as a counter
+    movia   r11, COUNTER                # initializing r11 as a counter
 TIMER_LOOP:
-    addi    r12, r12, -1                # decrementing COUNTER
-    beq     r12, r0, TIMER_LOOP         # COUNTER == 0 goto TIMER_LOOP
+    addi    r11, r11, -1                # decrementing COUNTER
+    beq     r11, r0, TIMER_LOOP         # COUNTER == 0 goto TIMER_LOOP
 
 /* Storing Z in the DATA field in the UART Control Register */
-    andi    r11, r11, Z
-    sthio   r11, 0(r8)                  # store ONLY the LOW halfword
+    andi    r10, r10, Z
+    sthio   r10, 0(r8)                  # store ONLY the LOW halfword
     br POLLING_UART
     
-END:
-	br END
-
 .end
